a515b2d1b83550297f301a5e493b29a3965d2903
Buvaneshwari <thirubuvana@gmail.com>
2023-02-10 19:52:37 +0530

    binaryTreeConstruct4

--- BinaryTree/constructBinaryTreeFromPreOrder&InOrder.js
+++ BinaryTree/constructBinaryTreeFromPreOrder&InOrder.js
@@ -11,7 +11,7 @@ function constructBTHelper(preOrder, preStart, preEnd, inOrder, inStart, inEnd) 
 
     let currentNode = new Node(preOrder[preStart]); //make the node with the data of preorder
     let nodeIndexInInorder = inOrder.indexOf(currentNode.data);
-    console.log("CREATED NODE : " ,currentNode) //get the index of current node data in inorder array...to find the left and right part
+    // console.log("CREATED NODE : " ,currentNode) //get the index of current node data in inorder array...to find the left and right part
     let numOfLeftElements = nodeIndexInInorder - inStart; //gives the number of right element
 
     currentNode.left = constructBTHelper(preOrder, preStart + 1, preStart + numOfLeftElements, inOrder, inStart, nodeIndexInInorder - 1)
@@ -20,7 +20,7 @@ function constructBTHelper(preOrder, preStart, preEnd, inOrder, inStart, inEnd) 
 }
 
 function constructBT(preOrder, inOrder) {
-    constructBTHelper(preOrder, 0, preOrder.length - 1, inOrder, 0, inOrder.length)
+    return constructBTHelper(preOrder, 0, preOrder.length - 1, inOrder, 0, inOrder.length)
 }
 let preOrder = [1, 2, 4, 8, 9, 10, 11, 5, 3, 6, 7] //node, left,right ...gives the next node
 let inOrder = [8, 4, 10, 9, 11, 2, 5, 1, 6, 3, 7]  //left ,node, right....gives the details of right and left node
